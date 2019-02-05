2b78b729f549658e8d67f9c4b47851ad8179124c
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2019-02-05 19:21:49 +0100

    teszt_branch_02 uj file and content

---
+++ teszt02.php
@@ -0,0 +1,6 @@
+<h2>Ez a teszt_branch_02 uj tartalom</h2>
+<p style="color: green;">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco.	
+</p>
